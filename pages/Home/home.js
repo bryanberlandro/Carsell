8a29d9e4f669ffadd2fa37860d62ccc2cb280e09
Berlandro <bryangodlive0@gmail.com>
2024-01-02 22:50:02 +0700

    add services section finished

--- pages/Home/home.js
+++ pages/Home/home.js
@@ -207,9 +207,28 @@ auditBtn[2].addEventListener('click', function(){
 })
 // CAR AUDIT SECTION 
 
+// SERVICES
+const servicesSection = document.querySelector('#services')
+const upReview = document.querySelector('.up-review')
+const downReview = document.querySelector('.down-review')
+
+function showMoreServices(){
+    if(servicesSection.classList.contains('h-max')){
+        servicesSection.classList.replace('h-max', 'h-[100vh]')
+        upReview.classList.replace('block', 'hidden')
+        downReview.classList.replace('hidden', 'block')
+    } else {
+        servicesSection.classList.replace('h-[100vh]', 'h-max')  
+        upReview.classList.replace('hidden', 'block')
+        downReview.classList.replace('block', 'hidden')
+    }
+}
+// SERVICES
+
 
 // REVIEW SECTION 
 let reviewWrapper = document.querySelector('.review-wrapper')
+
 
 document.addEventListener('DOMContentLoaded', async function(){
     const reviewData = await getReviewCarData()
@@ -224,10 +243,13 @@ function getReviewCarData(){
 
 function updateReviewCard(value){
     let wrapper = ""
-    value.forEach(val => {
-        wrapper += reviewCard(val)
-    });
-    reviewWrapper.innerHTML += wrapper
+    // value.forEach(val => {
+    //     wrapper += reviewCard(val)
+    // });
+    for(let i = 0; i < 3; i++){
+        let threeReview = value[i]
+        reviewWrapper.innerHTML += reviewCard(threeReview)
+    }
 }
 
 function reviewCard(value){
@@ -236,8 +258,16 @@ function reviewCard(value){
                     <img src="${value.image}" alt="" class="absolute w-full h-full object-cover">
                 </div>
                 <div class="px-[5%]">
-                    <h1 class="font-semibold">${value.name}</h1>
-                    <div class="flex gap-1">
+                    <div class="flex gap-2">
+                        <div class="relative w-7 h-7 rounded-full overflow-hidden">
+                            <img src="${value.userProfile}" class="absolute w-full"/>
+                        </div>
+                        <h1 class="font-semibold">${value.name}</h1>
+                    </div>
+                    <div class="flex gap-1 mt-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-yellow-300">
+                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                        </svg>                              
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 text-yellow-300">
                             <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
                         </svg>                              
